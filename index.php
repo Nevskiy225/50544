ae9e454decdfdb8651562fac0ad2134786ce121e
Nevskiy <kasatkindmitrij76@gmail.com>
2025-04-06 00:57:42 +0300

    немножко сократил код

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
 <?php
 $head_path = __DIR__ . "/includes/head.php";
 $header_path = __DIR__ . "/includes/header.php";
 $banner_path = __DIR__ . "/includes/banner.php";
 $popular_path = __DIR__ . "/includes/popular.php";
 $footer_path = __DIR__ . "/includes/footer.php";
-
-require_once $head_path;
-require_once $header_path;
-require_once $banner_path;
-require_once $popular_path;
-require_once $footer_path;
 ?>
+<head>
+    <title>50541</title>
+    <?php
+    require_once $head_path;
+    ?>
+    </head>
+<body>
+    <?php
+    require_once $header_path;
+    require_once $banner_path;
+    require_once $popular_path;
+    require_once $footer_path;
+    ?>
+    </body>
+</html>
